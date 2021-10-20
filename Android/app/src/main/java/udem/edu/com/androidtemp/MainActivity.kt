f4513837bfeb326c8b77eb57c4795bd00cac9ddb
alejou2009 <88717298+alejou2009@users.noreply.github.com>
2021-10-20 07:35:47 -0500

    Avance 2.0.4. Iniciando desarrollo app móvil.

---
+++ Android/app/src/main/java/udem/edu/com/androidtemp/MainActivity.kt
@@ -0,0 +1,11 @@
+package udem.edu.com.androidtemp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
